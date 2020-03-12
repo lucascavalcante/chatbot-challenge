430d6b4f75c066237ddca80024400a062408c976
Lucas <lucas.cavalcante@praxent.com>
2020-03-12 12:27:52 -0300

    First Commit - Add new application using docker-composer

---
+++ routes/botman.php
@@ -0,0 +1,13 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->fallback(function($bot) {
+    $bot->reply("I can't understand this message.");
+});
